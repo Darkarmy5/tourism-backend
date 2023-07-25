0108572c8af1aa3597894dd2af7760d3606f8665
Mudacumura Brunoblaise <brunoblaise94@gmail.com>
2023-07-25 20:38:39 +0200

    connection to db

--- index.js
+++ index.js
@@ -21,14 +21,26 @@ app.use(express.json());
 app.use(cors()); // allowing external app to access the api
 app.use(morgan("dev")); // log the request in the console
 
-mongoose
+/* mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
 
   .then(() => console.log("Connected to database successfully"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err)); */
+ 
+  mongoose.connect(`mongodb+srv://${process.env.username}:${process.env.password}@cluster0.xwadktq.mongodb.net/?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function(){
+    console.log('connection on')
+})
   app.use("/swagger", swaggerUi.serve,swaggerUi.setup(swaggerJson))
 app.use("/users", signUpRoute);
 app.use("/user", userRoute);
